ca5f425c5da2b3e77b7745ef79a676b3886f791a
Tkachenko-Alexander <mivotanaj@gmail.com>
2021-11-06 16:37:27 +0300

    Добавьте файлы проекта.

---
+++ PLLessons/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+*  ОПЕРАТОРЫ
+*  
+*  Арифметические операции с числами
+*/
+
+namespace PLLessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            int result =  a + 5;
+
+            Console.WriteLine(result);
+        }
+    } 
+}
